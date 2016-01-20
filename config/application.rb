a87b4a04e39a31de558edf1b4385acc51296f2f2
Aleksandr Karablin <aleks4ndr0@gmail.com>
2016-01-20 03:56:42 +0000

    added autoloading for app/services

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Shmoter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Custom directories with classes and modules you want to be autoloadable
+    config.autoload_paths += %W(#{config.root}/services)
   end
 end
