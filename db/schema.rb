360ef5df60ea8d4a8a2586aa40226e7979c12658
Aleksandr Karablin <aleks4ndr0@gmail.com>
2016-01-20 03:43:48 +0000

    changed database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160113031856) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.integer  "partner_id"
     t.string   "title"
